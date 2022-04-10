3409dd6a3cdf168c15e6340bd5f0ee57534b3826
oscar <opawong@yahoo.com>
2022-04-11 02:19:52 +1000

    done

--- client/src/components/App/Home.js
+++ client/src/components/App/Home.js
@@ -1,11 +1,11 @@
 function Home({ userName, userId }) {
   return (
-    <div>
+    <div className="home_title">
       <h1 className="slogen">Visualize your path {userName}</h1>
 
       <h3>
-        Keep tracking your hours spent, your to do list and the overall path.
-        Make things possible!!
+        Keep tracking your goal, your to do list and the overall path. Make
+        things possible!!
       </h3>
     </div>
   );
