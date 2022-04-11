f11d0d0cdc1fae631ab086767729c003374b9b70
oscar <opawong@yahoo.com>
2022-04-11 10:24:25 +1000

    add readme

--- client/src/components/App/Home.js
+++ client/src/components/App/Home.js
@@ -1,12 +1,15 @@
 function Home({ userName, userId }) {
   return (
-    <div className="home_title">
-      <h1 className="slogen">Visualize your path {userName}</h1>
+    <div>
+      <div className="home_title">
+        <h1 className="slogen">Visualize your path {userName}</h1>
 
-      <h3>
-        Keep tracking your goal, your to do list and the overall path. Make
-        things possible!!
-      </h3>
+        <h3>
+          Keep tracking your goal, your to do list and the overall path. Make
+          things possible!!
+        </h3>
+      </div>
+      <div className="img_box"></div>
     </div>
   );
 }
